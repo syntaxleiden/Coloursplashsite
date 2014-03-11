a7699952c288594dad6e7567cac9025f1f9711ef
Y0sh1 <joshua@theinternets.nl>
2014-03-11 17:46:54 +0100

    ajusting carousel and filling with images

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,6 @@
 $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+        scrollTop: $( $(this).attr('href') ).offset().top - 200
     }, 500);
     return false;
 });
